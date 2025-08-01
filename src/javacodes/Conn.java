31b32043271f159490572fcdf81dc8fd8b888b4b
Yaswanth <147583250+mudiliyaswanth@users.noreply.github.com>
2025-08-01 12:37:58 +0530

    Update Conn.java

--- src/javacodes/Conn.java
+++ src/javacodes/Conn.java
@@ -8,7 +8,7 @@ public class Conn{
     public Conn(){  
         try{  
             Class.forName("com.mysql.cj.jdbc.Driver");  
-            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","833YASwanth@");    
+            c =DriverManager.getConnection("jdbc:mysql://localhost:3306/bankmanagementsystem","root","password");    
             s =c.createStatement(); 
            
           
